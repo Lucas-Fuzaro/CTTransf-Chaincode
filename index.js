bd8322f5eba308e197c834a3df59598bc0ecc49d
Lucas Fuzaro <lfuzaro@outlook.com>
2019-03-14 18:16:08 -0300

    Clean up

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const shim = require("fabric-shim")
 const Dealer = require("./model/dealer")
 const Insurer = require("./model/insurer")
 const Registry = require("./model/registry")
-const utils = require('./model/utils')
 
 const Chaincode = class {
     async Init() {
@@ -30,38 +29,6 @@ const Chaincode = class {
             return shim.error(err)
         }
     }
-
-
-    // async checkingCar(stub, args){
-    //     // === Checking parameters ===
-    //     const plate = args[0].toUpperCase().replace(/[.,-\s]/g, '')
-    //     if (args.length !== 1) throw new Error("Incorrect number of arguments. This function accepts only 1 argument.")
-    //     if (plate.length <= 0 || plate == undefined || plate == null || plate !== 7) throw new Error ("Invalid Car Plate!")
-    //     if (!plate) throw new Error("Asset Identifier cannot be empty!")
-
-    //     if (id == undefined || id == null) throw new Error("Car ID must not be null or undefined!")
-    //     if (typeof owner !== "string" && owner.length == 0) throw new Error("Owner invalid!")
-    //     if (typeof new_owner !== "string" && new_owner.length == 0) throw new Error(`Owner ${new_owner} is not valid! It must be a string with at least 1 letter`)
-    //     if (new_owner.indexOf("@") < 0 || new_owner.indexOf("@") == new_owner.length - 1) throw new Error ("new_owner is invalid! Its format should be <UserIdentity>@<UserOrganization>")
-    //     if (new_owner.substring(new_owner.indexOf("@") + 1, new_owner.length - 1) !== "org" && typeof new_owner.substring(new_owner.length - 1) !== "number") throw new Error ("Invalid new_owner format!")
-
-    //     // === Checking if the car exists ===
-    //     let asset = await stub.getState(plate)
-    //     if (!asset && asset.length == 0) throw new Error("Asset does not exist!")
-    //     asset = JSON.parse(asset.toString())
-    //     if (asset.status !== "BEING_AVALIATED") throw new Error("Asset is not available for avaliation!")
-
-    //     // === Getting Registry Certificate ===
-    //     let registryCertificate = await utils.getCertificate(stub)
-    //     console.info("######", registryCertificate)
-
-    //     // === Updating Asset in the blockchain
-    //     await stub.putState(plate, Buffer.from(JSON.stringify(asset)))
-    //     console.info("Asset updated successfully")
-
-    //     return Buffer.from(JSON.stringify(plate))
-
-    // }
 
     async queryCar(stub, args) {
         let plate =  args[0].toUpperCase().replace(/[^a-z0-9]/gi,'')
